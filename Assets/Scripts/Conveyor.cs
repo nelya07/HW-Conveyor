cfd743ec2bf394e9b55054303135724d3202a39a
nelya07 <75816118+nelya07@users.noreply.github.com>
2020-12-16 00:57:40 +0300

    HW Conveyor

---
+++ Assets/Scripts/Conveyor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        collision.gameObject.transform.position += Vector3.left * _speed * Time.deltaTime;
+    }
+}
